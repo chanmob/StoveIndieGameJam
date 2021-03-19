21c75844fee8093f08de8827970f38209547de5d
chanmob <sellingout@naver.com>
2021-03-20 01:06:16 +0900

    스크립트 추가

---
+++ SpookyBoo/Assets/Scripts/Singleton/ObjectPoolManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : Singleton<ObjectPoolManager>
+{
+    public GameObject foodPrefab;
+    private Stack<GameObject> _stack_Food;
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+
+        _stack_Food = new Stack<GameObject>();
+    }
+
+    public GameObject GetFood()
+    {
+        int len = _stack_Food.Count;
+
+        if (len == 0)
+            MakeFood(1);
+
+        return _stack_Food.Pop(); ;
+    }
+
+    public void ReturnFood(GameObject food)
+    {
+        _stack_Food.Push(food);
+
+        if (food.activeSelf)
+            food.SetActive(false);
+    }
+
+    private void MakeFood(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            GameObject food = Instantiate(foodPrefab);
+            _stack_Food.Push(food);
+            food.SetActive(false);
+        }
+    }
+}
---
+++ SpookyBoo/Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    public Image[] heartImage;
+
+    public Text tailCount;
+
+    public Slider hungry;
+}
