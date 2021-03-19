c2a27b932de2f14e812ec401e6eaa31b7b3086aa
chanmob <sellingout@naver.com>
2021-03-20 02:10:39 +0900

    폰트추가, UI 스크립트 추가

---
+++ SpookyBoo/Assets/Scripts/CreateFood.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFood : MonoBehaviour
+{
+    public float createFoodTime = 3f;
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+            Debug.Log(Random.insideUnitCircle);
+    }
+
+    private IEnumerator CreateFoodCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(createFoodTime);
+        }
+    }
+}
--- SpookyBoo/Assets/Scripts/UI/MainUI.cs
+++ SpookyBoo/Assets/Scripts/UI/MainUI.cs
@@ -5,9 +5,36 @@ using UnityEngine.UI;
 
 public class MainUI : MonoBehaviour
 {
+    public Sprite[] heartSprite = new Sprite[2];
+
     public Image[] heartImage;
 
     public Text tailCount;
 
     public Slider hungry;
+
+    public void HeartRefresh(int curHp)
+    {
+        int len = heartImage.Length;
+
+        for(int i = 0; i < len; i++)
+        {
+            heartImage[i].sprite = heartSprite[0];
+        }
+
+        for (int i = 0; i < curHp; i++)
+        {
+            heartImage[i].sprite = heartSprite[1];
+        }
+    }
+
+    public void TailCountRefresh(int count, int max)
+    {
+        tailCount.text = count + " / " + max;
+    }
+
+    public void HungrySliderRefresh(float cur, float max)
+    {
+        hungry.value = cur / max;
+    }
 }
