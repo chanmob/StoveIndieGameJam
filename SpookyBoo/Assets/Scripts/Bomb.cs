3a63260abd6d0ac4696a36e0fb3d2b8e28821f3e
chanmob <sellingout@naver.com>
2021-03-20 13:48:44 +0900

    폭발 장애물 추가

---
+++ SpookyBoo/Assets/Scripts/Bomb.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject bomb;
+
+    private Animator _anim;
+
+    public float scaleTime;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+
+        StartCoroutine(BombCoroutine());
+    }
+
+    private IEnumerator BombCoroutine()
+    {
+        float time = 0f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime / scaleTime;
+            bomb.transform.localScale = new Vector2(time, time);
+
+            yield return null;
+        }
+
+        bomb.SetActive(false);
+        _anim.SetTrigger("Bomb");
+    }
+
+    public void BombAnimationFinish()
+    {
+        gameObject.SetActive(false);
+    }
+}
