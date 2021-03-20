8a605996b2dca22834f8ed0fe6abe7fc4872b682
chanmob <sellingout@naver.com>
2021-03-20 12:35:33 +0900

    적 생성기 추가

---
+++ SpookyBoo/Assets/Scripts/CreateEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour
+{
+    public float chaseRadius = 1f;
+    public float halfViewAngle = 30f;
+
+    public float viewZ = 0;
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 originPos = transform.position;
+
+        Gizmos.DrawWireSphere(originPos, chaseRadius);
+
+        Vector3 horizontalRightDir = AngleToDirZ(-halfViewAngle + viewZ);
+        Vector3 horizontalLeftDir = AngleToDirZ(halfViewAngle + viewZ);
+        Vector3 lookDir = AngleToDirZ(viewZ);
+
+        Debug.DrawRay(originPos, horizontalLeftDir * chaseRadius, Color.cyan);
+        Debug.DrawRay(originPos, lookDir * chaseRadius, Color.green);
+        Debug.DrawRay(originPos, horizontalRightDir * chaseRadius, Color.cyan);
+    }
+
+    private Vector2 AngleToDirZ(float angleInDegree)
+    {
+        float radian = (angleInDegree - transform.eulerAngles.z) * Mathf.Deg2Rad;
+        return new Vector2(Mathf.Sin(radian), Mathf.Cos(radian));
+    }
+}
