6b7c1c323b68df156b0e3f065aafeb281ffcd3ef
Park <mayer0425@naver.com>
2021-03-20 12:26:09 +0900

    move

---
+++ SpookyBoo/Assets/Scripts/moveCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class moveCharacter : MonoBehaviour
+{
+    
+    public float moveSpeed = 0.5f;
+    new Vector3 dir, m_scale;
+    public Camera mainCamera;
+    
+    void Update()
+    {
+        moveUpdate();
+        m_scale = gameObject.transform.localScale;
+        if (dir.x <= 0)
+        {
+            gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0);
+        }
+        if (dir.x >= 0)
+        {
+            gameObject.transform.localScale = new Vector3(-0.5f, 0.5f, 0);
+        }
+    }
+
+    void moveUpdate()
+    {
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+        dir = mousePos - gameObject.transform.position.normalized;
+        gameObject.transform.Translate(new Vector3(dir.x, dir.y, 0) * moveSpeed * Time.deltaTime);
+
+    }
+}
+
