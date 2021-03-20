091aef634e71e9ab3887736a421ffaef9ebeb6f3
Park <mayer0425@naver.com>
2021-03-20 13:10:11 +0900

    adf

---
+++ SpookyBoo/Assets/FoodEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+
+public class FoodEvent
+{
+    public delegate void foodHandler(int foodType);
+    public event foodHandler onEat;
+    public void eatFood(int foodType)
+    {
+        onEat(foodType);
+    }
+}
---
+++ SpookyBoo/Assets/FoodScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodScript : MonoBehaviour
+{
+    public int foodType;
+    FoodEvent foodEvent;
+    private void Start()
+    {
+        foodEvent = new FoodEvent();
+        foodEvent.onEat += new FoodEvent.foodHandler(onEatFood);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            foodEvent.eatFood(foodType);
+        }
+    }
+
+    void onEatFood(int foodType)
+    {
+        switch (foodType)
+        {
+            case 1:
+                Debug.Log("foodtype 1");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+    }
+}
