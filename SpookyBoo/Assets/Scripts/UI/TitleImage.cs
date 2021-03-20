d39b05b7ad59758a9b162fa438febb0e34b4a273
chanmob <sellingout@naver.com>
2021-03-20 19:58:03 +0900

    아웃게임 작업

---
+++ SpookyBoo/Assets/Scripts/UI/TitleImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleImage : MonoBehaviour
+{
+    public OutGameMainUI outgameMainUI;
+
+    public void TitleAnimationFinish()
+    {
+        outgameMainUI.CloudBtnOn();
+    }
+}
