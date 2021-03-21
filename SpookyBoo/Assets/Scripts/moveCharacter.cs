029c5080bd0aba6d5e487bd7a869a9245005a0e4
chanmob <sellingout@naver.com>
2021-03-21 13:26:31 +0900

    병합 주석 수정

--- SpookyBoo/Assets/Scripts/moveCharacter.cs
+++ SpookyBoo/Assets/Scripts/moveCharacter.cs
@@ -4,34 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 public class moveCharacter : MonoBehaviour
 {
-<<<<<<< HEAD
-    
-    public float moveSpeed = 0.5f;
-    new Vector3 dir, m_scale;
-    public Camera mainCamera;
-    
-    void Update()
-    {
-        moveUpdate();
-        m_scale = gameObject.transform.localScale;
-        if (dir.x <= 0)
-        {
-            gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0);
-        }
-        if (dir.x >= 0)
-        {
-            gameObject.transform.localScale = new Vector3(-0.5f, 0.5f, 0);
-        }
-    }
-
-    void moveUpdate()
-    {
-        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(mousePos);
-        dir = mousePos - gameObject.transform.position.normalized;
-        gameObject.transform.Translate(new Vector3(dir.x, dir.y, 0) * moveSpeed * Time.deltaTime);
-
-=======
     public GameObject BigBoo, foodCreater;
     public float moveSpeed = 0.5f, speedUp;
     public int n_needTail = 3;
@@ -202,7 +174,6 @@ public class moveCharacter : MonoBehaviour
     {
         UIManager.instance.gameOver.gameObject.SetActive(true);
         gameObject.SetActive(false);
->>>>>>> db653212c0b676d09570121afacf24cedcb8cf4b
     }
 }
 
